e553c3775b88dcec466854ed088b107efe2536b6
Flipps12 <paginasypruebas12@gmail.com>
2024-12-06 18:20:51 -0300

    Hostapd name of users

--- app/Services/hostapd.py
+++ app/Services/hostapd.py
@@ -1,24 +1,5 @@
 import subprocess
 
-def get_connected_users():
-    try:
-        # Ejecutar el comando hostapd_cli all_sta
-        output = subprocess.check_output(['sudo', 'hostapd_cli', 'all_sta']).decode()
-        clients = []
-        for line in output.splitlines():
-            # Las MACs son las únicas líneas formateadas como direcciones
-            if line and ':' in line and len(line.split(':')) == 6:
-                clients.append(line.strip())  # Extrae y limpia la MAC
-                get_hostname_from_ip(line.strip())
-                get_hostnames_from_dhcp_lease(line.strip())
-        return clients
-    except Exception as e:
-        print(f"Error: {e}")
-        return []
-
-# Llama a la función y muestra los resultados
-connected_users = get_connected_users()
-print("Usuarios conectados:", connected_users)
 
 def get_hostnames_from_dhcp_lease():
     lease_file = "/var/lib/misc/dnsmasq.leases"
@@ -43,3 +24,27 @@ def get_hostname_from_ip(ip_address):
         return hostname
     except socket.herror:
         return None
+
+
+
+def get_connected_users():
+    try:
+        # Ejecutar el comando hostapd_cli all_sta
+        output = subprocess.check_output(['sudo', 'hostapd_cli', 'all_sta']).decode()
+        clients = []
+        for line in output.splitlines():
+            # Las MACs son las únicas líneas formateadas como direcciones
+            if line and ':' in line and len(line.split(':')) == 6:
+                clients.append(line.strip())  # Extrae y limpia la MAC
+                get_hostname_from_ip(line.strip())
+                get_hostnames_from_dhcp_lease(line.strip())
+        return clients
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
+
+# Llama a la función y muestra los resultados
+connected_users = get_connected_users()
+print("Usuarios conectados:", connected_users)
+
+
