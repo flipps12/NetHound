159d68ff106d76d00848eb6f927ff49690f5a2a2
flipps12 <paginasypruebas12@gmail.com>
2025-05-25 22:41:25 -0300

    Nuevo Firewall

--- Firewall/src/main.rs
+++ Firewall/src/main.rs
@@ -1,147 +1,31 @@
 use dashmap::DashMap;
-use std::net::UdpSocket;
+use std::{fs, thread};
+use std::{net::UdpSocket, path::Path};
 use std::process::Command;
 use std::sync::Arc;
-use tokio::{io::AsyncBufReadExt, net::UnixListener};
+use tokio::{io::{AsyncBufReadExt, AsyncWriteExt}, net::{UnixListener, UnixStream}};
+use std::os::unix::net::UnixDatagram;
 use tokio::{
     io::BufReader,
     time::{self, Duration},
 };
+use std::collections::HashMap;
+use std::sync::mpsc::Sender;
+use std::sync::mpsc;
 
 const SERVER_WEB: &str = "http://localhost/api";
 const CHECK_INTERVAL: Duration = Duration::from_secs(5);
 
 #[derive(Debug)]
-struct Firewall {
-    allowed_ips: Arc<DashMap<String, bool>>, // IP -> Autorizada o no
+enum HostapdEvent {
+    Connected(String),    // MAC
+    Disconnected(String), // MAC
+    Other(String),        // Texto completo
 }
 
-impl Firewall {
-    fn new() -> Self {
-        let firewall = Firewall {
-            allowed_ips: Arc::new(DashMap::new()),
-        };
-        firewall.block_all();
-        firewall
-    }
-
-    fn block_all(&self) {
-        // Política por defecto: bloquear todo el tráfico reenviado
-        run_command("iptables", &["-P", "FORWARD", "DROP"]);
-
-        // Eliminar reglas existentes en FORWARD
-        run_command("iptables", &["-F", "FORWARD"]);
-
-        // Permitir tráfico de retorno de conexiones válidas
-        run_command(
-            "iptables",
-            &[
-                "-A",
-                "FORWARD",
-                "-m",
-                "conntrack",
-                "--ctstate",
-                "RELATED,ESTABLISHED",
-                "-j",
-                "ACCEPT",
-            ],
-        );
-
-        // Habilitar NAT para compartir internet
-        run_command(
-            "iptables",
-            &[
-                "-t",
-                "nat",
-                "-A",
-                "POSTROUTING",
-                "-o",
-                "eth0",
-                "-j",
-                "MASQUERADE",
-            ],
-        );
-
-        println!(
-            "🚫 Todo el tráfico está bloqueado salvo IPs autorizadas y conexiones existentes."
-        );
-    }
-
-    async fn check_authorization(&self, mac: &str, ip: &str) -> bool {
-        let url = format!("{}/verify?mac={}&ip={}", SERVER_WEB, mac, ip);
-        println!("🔗 Consultando: {}", url);
-
-        if let Ok(response) = reqwest::get(&url).await {
-            if let Ok(body) = response.text().await {
-                if let Ok(json) = serde_json::from_str::<serde_json::Value>(&body) {
-                    if let Some(authorized) = json.get("authorized").and_then(|v| v.as_bool()) {
-                        println!("🔑 Autorizado: {}", authorized);
-                        self.allowed_ips.insert(ip.to_string(), authorized);
-                        if authorized {
-                            self.unblock_ip(ip);
-                        }
-                        return authorized;
-                    }
-                }
-            }
-        }
-        false
-    }
-
-    async fn check_authorization_ip(&self, ip: &str) -> bool {
-        let url = format!("{}/onlyip?ip={}", SERVER_WEB, ip);
-
-        if let Ok(response) = reqwest::get(&url).await {
-            if let Ok(body) = response.text().await {
-                if let Ok(json) = serde_json::from_str::<serde_json::Value>(&body) {
-                    if let Some(authorized) = json.get("authorized").and_then(|v| v.as_bool()) {
-                        println!("🔑 Autorizado: {}", authorized);
-                        self.allowed_ips.insert(ip.to_string(), authorized);
-                        if authorized {
-                            self.unblock_ip(ip);
-                        } else {
-                            self.block_ip(ip);
-                        }
-                        return authorized;
-                    }
-                }
-            }
-        }
-        false
-    }
-
-    fn unblock_ip(&self, ip: &str) {
-        // Eliminamos posibles reglas repetidas previas
-        let _ = run_command(
-            "iptables",
-            &[
-                "-D", "FORWARD", "-s", ip, "-i", "wlan0", "-o", "eth0", "-j", "ACCEPT",
-            ],
-        );
-
-        // Insertamos la nueva regla
-        run_command(
-            "iptables",
-            &[
-                "-I", "FORWARD", "-s", ip, "-i", "wlan0", "-o", "eth0", "-j", "ACCEPT",
-            ],
-        );
-
-        println!("🚀 IP autorizada para pasar de wlan0 a eth0: {}", ip);
-    }
-
-    fn block_ip(&self, ip: &str) {
-        run_command(
-            "iptables",
-            &[
-                "-D", "FORWARD", "-s", ip, "-i", "wlan0", "-o", "eth0", "-j", "ACCEPT",
-            ],
-        );
-        println!(
-            "⛔️ IP bloqueada (acceso desde wlan0 a eth0 denegado): {}",
-            ip
-        );
-    }
+#[derive(Debug)]
+struct Firewall {
+    allowed_ips: Arc<DashMap<String, bool>>, // IP -> Autorizada o no
 }
 
 fn run_command(command: &str, args: &[&str]) {
@@ -168,74 +52,88 @@ fn get_private_ip() -> String {
     "0.0.0.0".to_string()
 }
 
-#[tokio::main]
-async fn main() {
-    let socket_path = "/tmp/net_hound.sock";
-    let firewall = Arc::new(Firewall::new());
 
-    println!("IP privada: {}", get_private_ip());
+fn parse_leases() -> std::io::Result<HashMap<String, String>> {
+    let content = std::fs::read_to_string("/var/lib/misc/dnsmasq.leases")?;
+    let mut map = HashMap::new();
 
-    if std::path::Path::new(socket_path).exists() {
-        std::fs::remove_file(socket_path).unwrap();
+    for line in content.lines() {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        if parts.len() >= 3 {
+            let mac = parts[1].to_string();
+            let ip = parts[2].to_string();
+            map.insert(mac, ip);
+        }
     }
 
-    let listener = UnixListener::bind(socket_path).unwrap();
-    println!("Servidor escuchando en: {}", socket_path);
+    Ok(map)
+}
 
-    let firewall_clone = Arc::clone(&firewall);
-    tokio::spawn(async move {
-        println!("🔄 Iniciando verificación de IPs...");
-        let mut interval = time::interval(CHECK_INTERVAL);
-        loop {
-            interval.tick().await;
-            println!("🔄 {:?}", firewall_clone.allowed_ips);
-            let ips: Vec<String> = firewall_clone
-                .allowed_ips
-                .iter()
-                .map(|entry| entry.key().clone())
-                .collect();
-            for ip in ips {
-                println!(
-                    "🔄 Verificando IP: {:?}",
-                    firewall_clone.check_authorization_ip(&ip).await
-                );
+
+fn hostapd_socket(tx: Sender<HostapdEvent>) -> std::io::Result<()> {
+    const CLIENT_PATH: &str = "/tmp/nethound.sock";
+    const HOSTAPD_SOCKET: &str = "/var/run/hostapd/wlan0";
+
+    if Path::new(CLIENT_PATH).exists() {
+        fs::remove_file(CLIENT_PATH)?;
+    }
+
+    let sock = UnixDatagram::bind(CLIENT_PATH)?;
+    sock.connect(HOSTAPD_SOCKET)?;
+    sock.send(b"ATTACH")?;
+
+    let mut buf = [0u8; 4096];
+    println!("[*] Escuchando eventos de hostapd...");
+
+    loop {
+        let size = sock.recv(&mut buf)?;
+        let msg = String::from_utf8_lossy(&buf[..size]);
+
+        if msg.contains("AP-STA-CONNECTED") {
+            if let Some(mac) = msg.split_whitespace().last() {
+                tx.send(HostapdEvent::Connected(mac.to_string())).ok();
             }
+        } else if msg.contains("AP-STA-DISCONNECTED") {
+            if let Some(mac) = msg.split_whitespace().last() {
+                tx.send(HostapdEvent::Disconnected(mac.to_string())).ok();
+            }
+        } else {
+            tx.send(HostapdEvent::Other(msg.to_string())).ok();
+        }
+    }
+}
+
+
+fn main() -> std::io::Result<()> {
+    let (tx, rx) = mpsc::channel();
+
+    // 🧵 Hilo para hostapd
+    thread::spawn(move || {
+        if let Err(e) = hostapd_socket(tx) {
+            eprintln!("[!] Error en hostapd_socket: {}", e);
         }
     });
 
+    // Hilo principal: escucha eventos y actúa
     loop {
-        match listener.accept().await {
-            Ok((stream, _)) => {
-                let firewall_clone = Arc::clone(&firewall);
-                tokio::spawn(async move {
-                    let reader = BufReader::new(stream);
-                    let mut lines = reader.lines();
+        if let Ok(event) = rx.recv_timeout(Duration::from_secs(1)) {
+            match event {
+                HostapdEvent::Connected(mac) => {
+                    println!("[+] Conectado: {}", mac);
 
-                    let mut current_ip = None;
-
-                    while let Ok(Some(line)) = lines.next_line().await {
-                        if current_ip.is_none() {
-                            current_ip = Some(line);
-                        } else {
-                            let mac = line;
-                            let ip = current_ip.take().unwrap();
-
-                            //println!("🔗 IP: {} MAC: {}", ip, mac);
-
-                            if !firewall_clone.allowed_ips.contains_key(&ip) {
-                            //     if *firewall_clone.allowed_ips.get(&ip).unwrap() {
-                            //         firewall_clone.unblock_ip(&ip);
-                            //     }
-                            // } else {
-                                firewall_clone.check_authorization(&mac, &ip).await;
-                                println!("New ip: {}", ip);
-                            }
+                    if let Ok(leases) = parse_leases() {
+                        if let Some(ip) = leases.get(&mac) {
+                            println!("→ IP asignada: {}", ip);
+                            // Ejecutar lógica de autorización, iptables, etc.
                         }
                     }
-                });
-            }
-            Err(e) => {
-                eprintln!("❌ Error al aceptar conexión: {}", e);
+                }
+                HostapdEvent::Disconnected(mac) => {
+                    println!("[-] Desconectado: {}", mac);
+                }
+                HostapdEvent::Other(msg) => {
+                    println!("[hostapd] {}", msg);
+                }
             }
         }
     }
